657ac2396275c49dd42bbb47cf62e335af8c89d0
Jung Insup <jisjis1218@gmail.com>
2021-09-27 21:26:52 +0900

    commit

--- springboot_kafka/src/main/java/com/insup/kafka/service/KafkaConsumer.java
+++ springboot_kafka/src/main/java/com/insup/kafka/service/KafkaConsumer.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "exam", groupId = "foo")
-    public void consume(String message) throws IOException {
-        System.out.println(String.format("Consumed message : %s", message));
-    }
+//    @KafkaListener(topics = "exam", groupId = "foo")
+//    public void consume(String message) throws IOException {
+//        System.out.println(String.format("Consumed message : %s", message));
+//    }
 }
