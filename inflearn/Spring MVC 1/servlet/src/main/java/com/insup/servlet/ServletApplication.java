19e3d3e4f7698fcd93352fb39a9d78c75abebc77
Jung Insup <jisjis1218@gmail.com>
2021-10-13 17:29:10 +0900

    commit

--- inflearn/Spring MVC 1/servlet/src/main/java/com/insup/servlet/ServletApplication.java
+++ inflearn/Spring MVC 1/servlet/src/main/java/com/insup/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package com.insup.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿 등록해준다.
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ inflearn/Spring MVC 1/servlet/src/main/java/com/insup/servlet/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.insup.servlet.basic;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+public class HelloData {
+
+    private String username;
+    private int age;
+}
