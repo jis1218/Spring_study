c2a882e85699473f65cd55f42b5058bec906803d
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-10-15 23:59:15 +0900

    commit

---
+++ inflearn/Spring MVC 1/springmvc/src/main/java/com/insup/hello/basic/requestmapping/MappingController.java
@@ -0,0 +1,20 @@
+package com.insup.hello.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+}
